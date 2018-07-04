ec3dad7ba7917cb9f509287c62b52e0713d0b74e
Hiedayah <ct_norhidayah94@yahoo.com>
2018-07-04 07:32:54 +0000

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+root "ideas#index"
+  resources :ideas
+  
+get "pages/info"
+	resources :ideas
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
